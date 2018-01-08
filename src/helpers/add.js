f00311c766188eaf054d6d480a054ae94c228132
Anton Hallstrom <antonhallstrom@live.se>
2018-01-08 20:02:28 +0100

    feat: add addNumbers with tests

---
+++ src/helpers/add.js
@@ -0,0 +1,13 @@
+
+/**
+ * Takes 2 numbers and return their sum.
+ * @param {number} a the first number
+ * @param {number} b the second number
+ *
+ * @returns {number} the sum of a and b
+ */
+function addNumbers(a, b) {
+  return a + b
+}
+
+export default addNumbers
