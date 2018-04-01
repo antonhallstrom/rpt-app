1647dee7cbbe2d2633fcabc07fafe695b0c69276
Anton Hallstrom <antonhallstrom@live.se>
2018-04-01 12:32:03 +0200

    WIP

--- src/components/workouts/create-exercice-form.js
+++ src/components/workouts/create-exercice-form.js
@@ -29,16 +29,70 @@ const Arrow = styled(Icon)`
   top: 50%;
   right: ${props => props.theme.spacing[0]};
   margin-top: -0.5em;
+  pointer-events: none;
+`
+
+const WeightUnit = styled.span`
+  position: absolute;
+  top: 50%;
+  right: ${props => props.theme.spacing[0]};
+  margin-top: -0.5em;
+  font-family: ${props => props.theme.fonts.body};
+`
+
+const InputWrapper = styled.div`
+  box-sizing: border-box;
+  border: 2px solid ghostwhite;
+  border-radius: 4px;
+`
+
+const Input = styled.input`
+  padding: ${props => props.theme.spacing[0]};
+  font-family: ${props => props.theme.fonts.body};
+  font-size: ${props => props.theme.fonts.sizes.large[0]};
+  box-sizing: border-box;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: ${props => props.theme.colors.black};
+  }
 `
 
 function CreateExerciceForm() {
   return (
-  <Wrapper>
-    <Select>
-      <option value={'Deadlift'}>Deadlift</option>
-    </Select>
-    <Arrow name="caret-down"/>
-  </Wrapper>
+    <div>
+      <Wrapper>
+        <Select name="exercices">
+          <option>Exercices</option>
+          <option value={'Deadlift'}>Deadlift</option>
+          <option value={'Deadlift'}>Squats</option>
+          <option value={'Deadlift'}>Bench Press</option>
+          <option value={'Deadlift'}>Overhead Press</option>
+          <option value={'Deadlift'}>Weighted Chinups</option>
+        </Select>
+        <Arrow name="caret-down"/>
+      </Wrapper>
+      <Wrapper>
+        <Select name="goal">
+          <option>Goal</option>
+          <option value={2}>2</option>
+          <option value={4}>4</option>
+          <option value={6}>6</option>
+          <option value={8}>8</option>
+          <option value={10}>10</option>
+          <option value={12}>12</option>
+        </Select>
+        <Arrow name="caret-down"/>
+      </Wrapper>
+      <Wrapper>
+        <InputWrapper>
+          <Input placeholder="Initial Load" />
+          <WeightUnit>kg</WeightUnit>
+        </InputWrapper>
+      </Wrapper>
+    </div>
   )
 }
 
